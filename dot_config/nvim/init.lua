5e9608c95e2809b68765b8221632c2129be832a6
Spencer <spencerraisanen@live.com>
2023-11-27 13:48:01 -0700

    chore: copilot auto

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -63,7 +63,7 @@ require('lazy').setup({
         },
         suggestion = {
           enabled = true,
-          auto_trigger = false,
+          auto_trigger = true,
           debounce = 75,
           keymap = {
             accept = "<C-l>",
