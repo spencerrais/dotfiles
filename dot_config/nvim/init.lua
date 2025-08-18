72bb2f9c515fc2e0d797680ada590aaefc3a2965
spencerrais <spencerraisanen@live.com>
2025-08-17 21:27:54 -0700

    chore: minor updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -11,17 +11,19 @@ vim.opt.wrap = false
 vim.opt.winborder = "rounded"
 
 vim.pack.add({
-	{ src = "https://github.com/navarasu/onedark.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/echasnovski/mini.icons" },
-	{ src = "https://github.com/echasnovski/mini.snippets" },
-	{ src = "https://github.com/echasnovski/mini.completion" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
-	{ src = "https://github.com/folke/which-key.nvim" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/m4xshen/hardtime.nvim" },
+        { src = "https://github.com/navarasu/onedark.nvim" },
+        { src = "https://github.com/echasnovski/mini.pick" },
+        { src = "https://github.com/echasnovski/mini.icons" },
+        { src = "https://github.com/echasnovski/mini.snippets" },
+        { src = "https://github.com/echasnovski/mini.completion" },
+        { src = "https://github.com/neovim/nvim-lspconfig" },
+        { src = "https://github.com/mason-org/mason.nvim" },
+        { src = "https://github.com/christoomey/vim-tmux-navigator" },
+        { src = "https://github.com/folke/which-key.nvim" },
+        { src = "https://github.com/lewis6991/gitsigns.nvim" },
+        { src = "https://github.com/m4xshen/hardtime.nvim" },
+        { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+        { src = "https://github.com/stevearc/conform.nvim" },
 })
 
 require "mini.pick".setup()
@@ -29,22 +31,35 @@ require "mini.icons".setup()
 require "mini.snippets".setup()
 require "mini.completion".setup()
 require "mason".setup()
-require("gitsigns").setup {
-	signs = { change = { text = '~' }, }
+require "gitsigns".setup {
+        signs = { change = { text = '~' }, }
 }
 require "hardtime".setup()
+require "nvim-treesitter.configs".setup({
+        ensure_installed = { "go", "lua", "python", "bash", "vim", "vimdoc" },
+        highlight = { enable = true },
+})
+require("conform").setup({
+        formatters_by_ft = {
+                python = { "isort", "ruff_format" },
+        },
+        format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+        },
+})
 
 vim.lsp.enable({ "lua_ls", "basedpyright" })
 vim.lsp.config("lua_ls",
-	{
-		settings = {
-			Lua = {
-				workspace = {
-					library = vim.api.nvim_get_runtime_file("", true),
-				}
-			}
-		}
-	})
+        {
+                settings = {
+                        Lua = {
+                                workspace = {
+                                        library = vim.api.nvim_get_runtime_file("", true),
+                                }
+                        }
+                }
+        })
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { desc = "update and source" })
 vim.keymap.set('n', '<leader>w', ':write<CR>')
