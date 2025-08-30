9f70f2e8c4005112d6d72bed824bb4f511be23d6
spencerrais <spencerraisanen@live.com>
2025-08-30 12:15:04 -0700

    chore: nvim and default .editorconfig

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,6 +9,11 @@ vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.wrap = false
 vim.opt.winborder = "rounded"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.confirm = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
 
 vim.pack.add({
         { src = "https://github.com/navarasu/onedark.nvim" },
@@ -27,25 +32,34 @@ vim.pack.add({
         { src = "https://github.com/stevearc/conform.nvim" },
 })
 
+-- mini ecosystem
 require "mini.pick".setup()
 require "mini.icons".setup()
 require "mini.snippets".setup()
 require "mini.completion".setup()
 require "mini.extra".setup()
+
+-- other plugins
 require "mason".setup()
+require "hardtime".setup()
 require "gitsigns".setup {
         signs = { change = { text = '~' }, }
 }
-require "hardtime".setup()
+
+-- treesitter
 require "nvim-treesitter.configs".setup({
         ensure_installed = { "go", "lua", "python", "bash", "vim", "vimdoc", "yaml" },
         sync_install = false,
         auto_install = true,
         highlight = { enable = true },
 })
+
+-- autoformatting
 require("conform").setup({
         formatters_by_ft = {
                 python = { "isort", "ruff_format" },
+                go = { "goimports", "gofumpt" },
+                json = { "jq" },
         },
         format_on_save = function(bufnr)
                 local ft = vim.bo[bufnr].filetype
@@ -56,7 +70,8 @@ require("conform").setup({
         end,
 })
 
-vim.lsp.enable({ "lua_ls", "basedpyright", "bashls", "yamlls" })
+-- lsp configurations
+vim.lsp.enable({ "lua_ls", "basedpyright", "bashls", "yamlls", "postgres_lsp" })
 vim.lsp.config("lua_ls",
         {
                 settings = {
@@ -67,18 +82,47 @@ vim.lsp.config("lua_ls",
                         }
                 }
         })
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { desc = "update and source" })
+
+-- movement
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'half page up and center' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'half page down and center' })
+
+-- file changes
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { desc = 'update and source' })
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>ff', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+-- finder for files, buffers, help, lsp symbols, marks
+vim.keymap.set('n', '<leader>ff', ":Pick files sort=visit_recent<CR>")
 vim.keymap.set('n', '<leader>fb', ":Pick buffers<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 vim.keymap.set('n', '<leader>fs', ":Pick lsp scope='document_symbol'<CR>", { desc = 'Pick current buffer LSP symbols' })
 vim.keymap.set('n', '<leader>fS', ":Pick lsp scope='workspace_symbol'<CR>", { desc = 'Pick workspace LSP symbols' })
 vim.keymap.set('n', '<leader>fm', ':Pick marks scope="buf"<CR>', { desc = 'Pick marks (current buffer)' })
 vim.keymap.set('n', '<leader>fM', ':Pick marks<CR>', { desc = 'Pick marks (global)' })
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 
+-- global clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
+
+-- highlight yanked text briefly
+vim.api.nvim_create_autocmd("TextYankPost", {
+        desc = "Highlight selection on yank",
+        group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+        callback = function()
+                vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+        end,
+})
+
+-- diagnostic configuration: toggle with <leader>d
+vim.g.diagnostics_virtual_lines = true
+local function toggle_virtual_lines()
+        vim.g.diagnostics_virtual_lines = not vim.g.diagnostics_virtual_lines
+        vim.diagnostic.config({
+                virtual_lines = vim.g.diagnostics_virtual_lines,
+        })
+end
+vim.keymap.set("n", "<leader>d", toggle_virtual_lines, { desc = "Toggle diagnostics (virtual_lines)" })
 
 vim.cmd("colorscheme onedark")
