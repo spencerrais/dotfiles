3ee75dcd65c7207e4b6bcb3a63c2725594daa0d3
Spencer <spencerraisanen@live.com>
2026-06-14 00:12:43 -0700

    feat: move to mise, reduce complexity (#41)
    * feat: move to mise and reduced complexity
    
    * chore: minor updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,20 +16,20 @@ vim.opt.timeoutlen = 300
 vim.opt.undofile = true
 
 vim.pack.add({
-        { src = "https://github.com/navarasu/onedark.nvim" },
-        { src = "https://github.com/echasnovski/mini.pick" },
-        { src = "https://github.com/echasnovski/mini.icons" },
-        { src = "https://github.com/echasnovski/mini.snippets" },
-        { src = "https://github.com/echasnovski/mini.completion" },
-        { src = "https://github.com/echasnovski/mini.extra" },
-        { src = "https://github.com/neovim/nvim-lspconfig" },
-        { src = "https://github.com/mason-org/mason.nvim" },
-        { src = "https://github.com/christoomey/vim-tmux-navigator" },
-        { src = "https://github.com/folke/which-key.nvim" },
-        { src = "https://github.com/lewis6991/gitsigns.nvim" },
-        { src = "https://github.com/m4xshen/hardtime.nvim" },
-        { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-        { src = "https://github.com/stevearc/conform.nvim" },
+  { src = "https://github.com/navarasu/onedark.nvim" },
+  { src = "https://github.com/echasnovski/mini.pick" },
+  { src = "https://github.com/echasnovski/mini.icons" },
+  { src = "https://github.com/echasnovski/mini.snippets" },
+  { src = "https://github.com/echasnovski/mini.completion" },
+  { src = "https://github.com/echasnovski/mini.extra" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/christoomey/vim-tmux-navigator" },
+  { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
+  { src = "https://github.com/m4xshen/hardtime.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/stevearc/conform.nvim" },
 })
 
 -- mini ecosystem
@@ -43,45 +43,45 @@ require "mini.extra".setup()
 require "mason".setup()
 require "hardtime".setup()
 require "gitsigns".setup {
-        signs = { change = { text = '~' }, }
+  signs = { change = { text = '~' }, }
 }
 
 -- treesitter
 require "nvim-treesitter.configs".setup({
-        ensure_installed = { "go", "lua", "python", "bash", "vim", "vimdoc", "yaml" },
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
+  ensure_installed = { "go", "lua", "python", "bash", "yaml" },
+  sync_install = false,
+  auto_install = true,
+  highlight = { enable = true },
 })
 
 -- autoformatting
 require("conform").setup({
-        formatters_by_ft = {
-                python = { "isort", "ruff_format" },
-                go = { "goimports", "gofumpt" },
-                json = { "jq" },
-        },
-        format_on_save = function(bufnr)
-                local ft = vim.bo[bufnr].filetype
-                if ft == "yaml" then
-                        return nil
-                end
-                return { timeout_ms = 5000, lsp_format = "fallback" }
-        end,
+  formatters_by_ft = {
+    python = { "isort", "ruff_format" },
+    go = { "goimports", "gofumpt" },
+    json = { "jq" },
+  },
+  format_on_save = function(bufnr)
+    local ft = vim.bo[bufnr].filetype
+    if ft == "yaml" then
+      return nil
+    end
+    return { timeout_ms = 5000, lsp_format = "fallback" }
+  end,
 })
 
 -- lsp configurations
 vim.lsp.enable({ "lua_ls", "basedpyright", "bashls", "yamlls", "postgres_lsp" })
 vim.lsp.config("lua_ls",
-        {
-                settings = {
-                        Lua = {
-                                workspace = {
-                                        library = vim.api.nvim_get_runtime_file("", true),
-                                }
-                        }
-                }
-        })
+  {
+    settings = {
+      Lua = {
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+        }
+      }
+    }
+  })
 
 -- movement
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'half page up and center' })
@@ -108,21 +108,11 @@ vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard"
 
 -- highlight yanked text briefly
 vim.api.nvim_create_autocmd("TextYankPost", {
-        desc = "Highlight selection on yank",
-        group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-        callback = function()
-                vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-        end,
+  desc = "Highlight selection on yank",
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
 })
-
--- diagnostic configuration: toggle with <leader>d
-vim.g.diagnostics_virtual_lines = true
-local function toggle_virtual_lines()
-        vim.g.diagnostics_virtual_lines = not vim.g.diagnostics_virtual_lines
-        vim.diagnostic.config({
-                virtual_lines = vim.g.diagnostics_virtual_lines,
-        })
-end
-vim.keymap.set("n", "<leader>d", toggle_virtual_lines, { desc = "Toggle diagnostics (virtual_lines)" })
 
 vim.cmd("colorscheme onedark")
